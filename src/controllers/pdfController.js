016b5bbb3c03635bd56e9f533255d56aad18c947
hemantmp <hemant.jadhav@mobileprogramming.com>
2023-11-03 12:35:27 +0530

    not working code

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -1,18 +1,21 @@
+const { createPdfFile } = require("../helper/pdfcreatefile");
+
 const asyncHandler = require("express-async-handler");
 // const Contact = require("../models/contactModel");
 
 const getAllPdfs = (req, res) => {
   res.json({ message: "this is get all pdf" });
 };
+
 const createPdf = asyncHandler(async (req, res) => {
   console.log("The request body is :", req.body);
+   createPdfFile();
   //   const { name, email, phone } = req.body;
   //   if (!name || !email || !phone) {
   //     res.status(400);
   //     throw new Error("All fields are mandatory !");
   //   }
 
-  res.status(201).json("pdf");
 });
 
 const getOnePdf = (req, res) => {
--- src/helper/pdfcreatefile.js
+++ src/helper/pdfcreatefile.js
@@ -3,46 +3,48 @@ const fs = require("fs");
 const doc = new PDFDocument();
 
 const createPdfFile = ({ pdfName = "example3" }) => {
-  return new Promise((resolve, reject) => {
-    try {
-      doc.pipe(fs.createWriteStream(`../storage/pdfs/${pdfName}.pdf`));
+    console.log('file created started')
+    // return new Promise((resolve, reject) => {
+    //     try {
+            doc.pipe(fs.createWriteStream(`example3.pdf`));
 
-      doc.fontSize(27).text("This the article by Hemant Jadhav", 100, 100);
+            doc.fontSize(27).text("This the article by Hemant Jadhav", 100, 100);
 
-      // Adding an image in the pdf.
-      // doc.image('download3.jpg', {
-      // 	fit: [300, 300],
-      // 	align: 'center',
-      // 	valign: 'center'
-      // });
+            // Adding an image in the pdf.
+            // doc.image('download3.jpg', {
+            // 	fit: [300, 300],
+            // 	align: 'center',
+            // 	valign: 'center'
+            // });
 
-      doc
-        .addPage()
-        .fontSize(15)
-        .text("Generating PDF with the provide name", 100, 100);
+            doc
+                .addPage()
+                .fontSize(15)
+                .text("Generating PDF with the provide name", 100, 100);
 
-      // Apply some transforms and render an SVG path with the
-      doc
-        .scale(0.6)
-        .translate(470, -380)
-        .path("M 250,75 L 323,301 131,161 369,161 177,301 z")
-        .fill("red", "even-odd")
-        .restore();
+            // Apply some transforms and render an SVG path with the
+            doc
+                .scale(0.6)
+                .translate(470, -380)
+                .path("M 250,75 L 323,301 131,161 369,161 177,301 z")
+                .fill("red", "even-odd")
+                .restore();
 
-      // Add some text with annotations
-      doc
-        .addPage()
-        .fillColor("blue")
-        .text("The link for my portfolio website", 100, 100)
-        .link(100, 100, 160, 27, "https://crazy-devloper.vercel.app");
+            // Add some text with annotations
+            doc
+                .addPage()
+                .fillColor("blue")
+                .text("The link for my portfolio website", 100, 100)
+                .link(100, 100, 160, 27, "https://crazy-devloper.vercel.app");
 
-      // Finalize PDF file
-      doc.end();
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+            // Finalize PDF file
+            doc.end();
+            console.log('file created successfull')
+            // resolve();
+        // } catch (error) {
+        //     reject(error);
+        // }
+    // });
 };
 
 module.exports = { createPdfFile };
